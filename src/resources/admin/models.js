f04b3bcc399f3bffc39864fe088b370fdb5575ad
Zabcode <hunter.eal@yandex.ru>
2019-03-01 22:30:48 +0300

    convinient way to use models

---
+++ src/resources/admin/models.js
@@ -0,0 +1,5 @@
+const User = require("../user/user.model");
+const Post = require("../post/post.model");
+const Comment = require("../post/comment/comment.model");
+
+module.exports = [User, Post, Comment];
