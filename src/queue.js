40b5506624bc75a967db79fc9673775416fac2a9
Ilya <44939563+zabozlaev@users.noreply.github.com>
2019-03-17 12:31:18 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,15 +1,4 @@
-// const kue = require("kue");
 
-// const queue = kue.createQueue();
-
-// process.once("SIGTERM", sig => {
-//   queue.shutdown(5000, err => {
-//     console.log("Kue shutdown: ", err || "");
-//     process.exit(0);
-//   });
-// });
-
-// module.exports = queue;
 
 const Queue = require("bull");
 
